a9f5ead92f5d67e11e229b2c9757f605ce81c71d
suraj <surajssc1232@gmail.com>
2025-03-29 16:47:00 +0530

    changed something in the dotfiles

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,7 +6,7 @@ DOTFILES_DIR="$(pwd)"
 # Define the target directories where the dotfiles should go
 TARGET_ZSHRC_DIR="$HOME"
 TARGET_LUA_DIR="$HOME/.config/nvim"  # Change this if you store init.lua somewhere else
-TARGET_STARSHIP_DIR="$HOME/.config/starship"  # Change this if necessary
+TARGET_STARSHIP_DIR="$HOME/.config"  # Change this if necessary
 
 # Ensure the target directories exist
 mkdir -p "$TARGET_LUA_DIR"
