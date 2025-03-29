793ebc1d95eb1e9cf5e7fddda6a48ff8e1fb7a1c
suraj <surajssc1232@gmail.com>
2025-03-29 16:50:32 +0530

    added both ghostty and tmux

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,15 +7,19 @@ DOTFILES_DIR="$(pwd)"
 TARGET_ZSHRC_DIR="$HOME"
 TARGET_LUA_DIR="$HOME/.config/nvim"  # Change this if you store init.lua somewhere else
 TARGET_STARSHIP_DIR="$HOME/.config"  # Change this if necessary
+TARGET_TMUX_DIR="$HOME"
+TARGET_GHOSTTY_DIR="$HOME/.config/ghostty"
 
 # Ensure the target directories exist
 mkdir -p "$TARGET_LUA_DIR"
 mkdir -p "$TARGET_STARSHIP_DIR"
-
+mkdir -p "$TARGET_GHOSTTY_DIR"
 # Copy the files, replacing any existing files
 cp -f "$DOTFILES_DIR/init.lua" "$TARGET_LUA_DIR/init.lua"
 cp -f "$DOTFILES_DIR/.zshrc" "$TARGET_ZSHRC_DIR/.zshrc"
 cp -f "$DOTFILES_DIR/starship.toml" "$TARGET_STARSHIP_DIR/starship.toml"
+cp -f "$DOTFILES_DIR/.tmux.conf" "$TARGET_TMUX_DIR/.tmux.conf"
+cp -f "$DOTFILES_DIR/ghostty/config" "$TARGET_GHOSTTY_DIR/config"
 
 echo "Dotfiles have been placed successfully!"
 
