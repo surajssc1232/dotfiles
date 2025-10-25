9d736f842ddba8fd8b6bf7eadf14f05e1e5bdf32
suraj <surajssc1232@gmail.com>
2025-10-25 23:14:01 +0530

    added fish and mango

--- fish/config.fish
+++ fish/config.fish
@@ -1,32 +1,32 @@
+set -gx TERM xterm-256color
+set -gx EDITOR nvim
+
 if status is-interactive
     set fish_greeting
     fastfetch --config examples/9.jsonc
 
-		# ${UserConfigDir}/fish/config.fish
-		set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-		carapace _carapace | source
+    # ${UserConfigDir}/fish/config.fish
+    # set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
+    # carapace _carapace | source
 
+    zoxide init fish | source
 
-		zoxide init fish | source
-		
-		set -x NNN_OPTS d
+    set -x NNN_OPTS d
 
+    if type -q tmux
+        if status is-interactive
+            and test -z "$TMUX"
+            and string match -qv "screen*" $TERM
+            and string match -qv "tmux*" $TERM
+            exec tmux
+        end
+    end
 
-		if type -q tmux
-				if status is-interactive
-						and test -z "$TMUX"
-						and string match -qv "screen*" $TERM
-						and string match -qv "tmux*" $TERM
-								exec tmux
-				end
-		end
+    alias hh helix
+    alias hx helix
 
-		alias hh "helix"
-		alias hx "helix"
-
-
-		# FZF default options (Fish)
-		set -gx FZF_DEFAULT_OPTS "\
+    # FZF default options (Fish)
+    set -gx FZF_DEFAULT_OPTS "\
 		--color=bg+:#202020,bg:#151515,spinner:#ffafaf,hl:#ff8700 \
 		--color=fg:#dddddd,header:#ffaf5f,info:#ff8700,pointer:#ffafaf \
 		--color=marker:#ff5f87,fg+:#c6b6ee,prompt:#ff8700,hl+:#ff8700 \
